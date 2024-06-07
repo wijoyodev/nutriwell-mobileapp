6d70b907e825abcd610e5eff147454862ac29d19
Yahya <yahya@gdn-commerce.com>
2024-06-07 23:00:56 +0700

    setup environment

--- android/app/src/main/java/com/nutriwell/garena/MainActivity.kt
+++ android/app/src/main/java/com/nutriwell/garena/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.nutriwell
+package com.nutriwell.garena
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/com/nutriwell/garena/MainApplication.kt
+++ android/app/src/main/java/com/nutriwell/garena/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.nutriwell
+package com.nutriwell.garena
 
 import android.app.Application
 import com.facebook.react.PackageList
@@ -11,6 +11,7 @@ import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.flipper.ReactNativeFlipper
 import com.facebook.soloader.SoLoader
+import com.nutriwell.garena.BuildConfig
 
 class MainApplication : Application(), ReactApplication {
 
